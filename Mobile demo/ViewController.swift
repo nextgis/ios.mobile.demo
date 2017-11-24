26ddbf634369ad83dfc8c47cce7f0b11cd7bb9a5
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-11-25 02:03:55 +0300

    Add create new vector layer and fill points.

--- Mobile demo/ViewController.swift
+++ Mobile demo/ViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 import ngmaplib
 
-class ViewController: UIViewController, GestureDelegate, URLSessionDownloadDelegate, MapViewDelegate {
+class ViewController: UIViewController, GestureDelegate {
     
     // MARK: Properties
     
-    @IBOutlet weak var featureCountLabel: UILabel!
     @IBOutlet weak var mapView: MapView!
-    @IBOutlet weak var downloadBtn: UIButton!
-    let pointsURL = "https://nextgis.com/data/examples/store.ngst"
     weak var map: Map?
     var pointsFC: FeatureClass? = nil
-    weak var globalTimer: Timer? = nil
     
     // MARK: Overrides
 
@@ -45,21 +41,15 @@ class ViewController: UIViewController, GestureDelegate, URLSessionDownloadDeleg
                                 maxX: 20037508.34,
                                 maxY: 20037508.34)
             if map.layerCount == 0 { // Map is just created   
-                if API.instance.getStore("store") != nil {
-                    addPoints(to: map)
-                    downloadBtn.isHidden = true
-                }
+                addPoints(to: map)
                 addOSM(to: map)
 
                 
                 _ = map.save()
-            } else if map.layerCount >= 2 {
-                downloadBtn.isHidden = true
             }
         
             mapView.setMap(map: map)
             mapView.registerGestureRecognizers(self)
-            mapView.registerView(self)
             
             self.map = map
         }
@@ -93,10 +83,10 @@ class ViewController: UIViewController, GestureDelegate, URLSessionDownloadDeleg
     func addOSM(to map: Map) {
         if let dataDir = API.instance.getDataDirectory() {
             
-            let bbox = BBox(minx: -20037508.34,
-                            miny: -20037508.34,
-                            maxx: 20037508.34,
-                            maxy: 20037508.34)
+            let bbox = Envelope(minX: -20037508.34,
+                                minY: -20037508.34,
+                                maxX: 20037508.34,
+                                maxY: 20037508.34)
             let baseMap = dataDir.createTMS(name: "osm.wconn",
                                         url: "http://tile.openstreetmap.org/{z}/{x}/{y}.png",
                                         epsg: 3857,
@@ -112,11 +102,50 @@ class ViewController: UIViewController, GestureDelegate, URLSessionDownloadDeleg
     func addPoints(to map: Map) {
         // Get or create data store
         if let dataStore = API.instance.getStore("store") {
-            // We expected datastore has points feature class
-            if let pointsFC = dataStore.child(name: "points") {
-                // Get OSM Layer
-                let osmLayer = map.getLayer(by: 0)
+            // Create points feature class
+            
+            let options = [
+                "CREATE_OVERVIEWS" : "ON",
+                "ZOOM_LEVELS" : "2,3,4,5,6,7,8,9,10,11,12,13,14"
+            ]
+            
+            let fields = [
+                Field(name: "long", alias: "long", type: .REAL),
+                Field(name: "lat", alias: "lat", type: .REAL),
+                Field(name: "datetime", alias: "datetime", type: .DATE, defaultValue: "CURRENT_TIMESTAMP"),
+                Field(name: "name", alias: "name", type: .STRING)
+            ]
+            
+            if let pointsFC = dataStore.createFeatureClass(name: "points",
+                                                           geometryType: .POINT,
+                                                           fields: fields,
+                                                           options: options) {
+            
+                // Add geodata to points feature class from https://en.wikipedia.org
+                let coordinates = [
+                    (name: "Moscow", x: 37.616667, y: 55.75),
+                    (name: "London", x: -0.1275, y: 51.507222),
+                    (name: "Washington", x: -77.016389, y: 38.904722),
+                    (name: "Beijing", x: 116.383333, y: 39.916667),
+                ]
                 
+                let coordTransform = CoordinateTransformation.new(fromEPSG: 4326, toEPSG: 3857)
+                
+                for coordinate in coordinates {
+                    if let feature = pointsFC.createFeature() {
+                        if let geom = feature.createGeometry() as? GeoPoint {
+                            let point = Point(x: coordinate.x, y: coordinate.y)
+                            let transformPoint = coordTransform.transform(point)
+                            geom.setCoordinates(point: transformPoint)
+                            feature.geometry = geom
+                            feature.setField(for: 0, double: coordinate.x)
+                            feature.setField(for: 1, double: coordinate.y)
+                            feature.setField(for: 3, string: coordinate.name)
+                            _ = pointsFC.insertFeature(feature)
+                        }
+                    }
+                }
+
                 // Create layer from points feature class
                 if let pointsLayer = map.addLayer(name: "Points", source: pointsFC) {
                 
@@ -127,13 +156,10 @@ class ViewController: UIViewController, GestureDelegate, URLSessionDownloadDeleg
                                   color: UIColor(red: 0.0, green: 0.8, blue: 0.545,
                                                  alpha: 1.0)), for: "color")
                     
-                    _ = style.set(double: 5.0, for: "size")
-                    _ = style.set(int: 4, for: "type")
+                    _ = style.set(double: 8.0, for: "size")
+                    _ = style.set(int: 6, for: "type") // Star symbol
                     
                     pointsLayer.style = style
-                
-                    map.reorder(before: osmLayer, moved: pointsLayer)
-                    _ = map.save()
                 }
             }
         }
@@ -143,45 +169,7 @@ class ViewController: UIViewController, GestureDelegate, URLSessionDownloadDeleg
         return map?.getLayer(by: 0)?.dataSource as? FeatureClass
     }
     
-    func updateFeatureCountLabel() {
-        if let fc = getPointsFeatureClass() {
-            let queue = DispatchQueue.global(qos: .utility)
-            queue.async{
-                let extent = self.mapView.getExtent(srs: 3857)
-                _ = fc.setSpatialFilter(envelope: extent)
-                let count = fc.count
-                //_ = fc.clearFilters()
-            
-                let formatter = NumberFormatter()
-                formatter.groupingSeparator = " "
-                formatter.numberStyle = .decimal
-                let formatStr = formatter.string(for: count) ?? "0"
-                DispatchQueue.main.async {
-                    self.featureCountLabel.text = "\(formatStr) features"
-                }
-            }
-        }
-    }
-    
-    func onTimer(timer: Timer) {
-        updateFeatureCountLabel()
-        globalTimer = nil
-    }
-    
-    func scheduleUpdateLabel() {
-        
-        if globalTimer != nil {
-            return
-        }
-        
-        globalTimer = Timer.scheduledTimer(timeInterval: 2.95,
-                                           target: self,
-                                           selector: #selector(onTimer(timer:)),
-                                           userInfo: nil,
-                                           repeats: false)
-    }
-
-
+   
     // MARK: Actions
     
     @IBAction func onInfo(_ sender: UIBarButtonItem) {
@@ -217,65 +205,6 @@ class ViewController: UIViewController, GestureDelegate, URLSessionDownloadDeleg
     
     @IBAction func onZoomOut(_ sender: UIButton) {
         mapView.zoomOut()
-    }
-    
-    @IBAction func onDownload(_ sender: UIButton) {
-        sender.isEnabled = false
-        let sessionConfig = URLSessionConfiguration.default
-        let session = URLSession(configuration: sessionConfig, delegate: self,
-                                 delegateQueue: OperationQueue.main)
-        let request = try! URLRequest(url: URL(string: pointsURL)!)
-        
-        let task = session.downloadTask(with: request)
-        task.resume()
-    }
-    
-    var progress: Float = 0.0
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        if totalBytesExpectedToWrite > 0 {
-            let currentProgress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite) * 100
-            debugPrint("Progress \(downloadTask) \(currentProgress)")
-            if currentProgress - progress > 0.25 {
-                progress = currentProgress
-                let progressStr = String(format: "%.1f", progress)
-                downloadBtn.setTitle("Get points: \(progressStr)%", for: .disabled)
-            }
-        }
-    }
-    
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        debugPrint("Download finished: \(location)")
-        let fileManager = FileManager.default
-        var appSupportDir = fileManager.urls(for: .applicationSupportDirectory,
-                                             in: .userDomainMask)[0]
-        appSupportDir = appSupportDir.appendingPathComponent("ngstore", isDirectory: true)
-        appSupportDir = appSupportDir.appendingPathComponent("geodata", isDirectory: true)
-        let localUrl = appSupportDir.appendingPathComponent("store.ngst")
-        
-        do {
-            try fileManager.removeItem(at: localUrl)
-        } catch( _) {
-            
-        }
-            
-        do {
-            debugPrint("Try to move from \(location) to \(localUrl)")
-            try fileManager.moveItem(at: location, to: localUrl)
-            self.addPoints(to: self.map!)
-            self.mapView.refresh(normal: true)
-            downloadBtn.isHidden = true
-        } catch (let writeError) {
-            debugPrint("error writing file \(localUrl) : \(writeError)")
-        }
-    }
-    
-    func onMapDrawFinished() {
-        scheduleUpdateLabel()
-    }
-    
-    func onMapDraw(percent: Double) {
-        
     }
 }
 
